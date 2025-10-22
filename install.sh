a8179d54f1a6a61ea35b3efb2b9541b499513c07
Rathes Sachchithananthan <hello@rathes.me>
2025-10-22 20:31:26 +0100

    🐛 Remove unused tap

--- install.sh
+++ install.sh
@@ -26,7 +26,6 @@ ln -sw $HOME/.dotfiles/.zshrc $HOME/.zshrc
 brew update
 
 # Install all our dependencies with bundle (See Brewfile)
-brew tap homebrew/bundle
 brew bundle
 
 # Make ZSH the default shell environment
